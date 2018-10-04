ef40d14ef75addac8592744777e6367334acc623
bold <bold@cryptoguru.com>
2018-10-05 01:41:40 +0800

    use safe randmap

--- pkg/p2p/peer_manager.go
+++ pkg/p2p/peer_manager.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	pb "github.com/ac0v/aspera/internal/api/protobuf-spec"
-	"github.com/lukechampine/randmap"
+	"github.com/lukechampine/randmap/safe"
 )
 
 var hasPortRegexp = regexp.MustCompile(":([1-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$")
