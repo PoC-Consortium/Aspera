533aa14ba1ac68abdcab4fa62e078febb5a7ed8f
bold <bold@cryptoguru.com>
2018-10-18 03:40:33 +0800

    differ between encoding of message content (string and hex string)

--- pkg/transaction/attachment/message.go
+++ pkg/transaction/attachment/message.go
@@ -3,11 +3,13 @@ package attachment
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 
 	"github.com/ac0v/aspera/pkg/parsing"
-
-	"gopkg.in/restruct.v1"
+	"github.com/json-iterator/go"
 )
+
+var js = jsoniter.ConfigCompatibleWithStandardLibrary
 
 type Message struct {
 	IsText *bool `struct:"-" json:"messageIsText,omitempty"`
@@ -16,6 +18,26 @@ type Message struct {
 	IsTextAndLen int32  `json:"-"`
 	Content      string `json:"message,omitempty"`
 	Version      int8   `struct:"-" json:"version.Message,omitempty"`
+}
+
+func (m *Message) UnmarshalJSON(bs []byte) error {
+	type Alias Message
+	ma := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(m),
+	}
+	if err := js.Unmarshal(bs, ma); err != nil {
+		return err
+	}
+
+	if m.IsText != nil && *m.IsText {
+		m.IsTextAndLen = int32(-len(m.Content))
+	} else {
+		m.IsTextAndLen = int32(len(m.Content) / 2)
+	}
+
+	return nil
 }
 
 func (attachment *Message) FromBytes(bs []byte, version uint8) (int, error) {
@@ -33,20 +55,40 @@ func (attachment *Message) FromBytes(bs []byte, version uint8) (int, error) {
 	if err := binary.Read(r, binary.LittleEndian, &content); err != nil {
 		return 0, err
 	}
-	attachment.Content = string(content)
+	if isText {
+		attachment.Content = string(content)
+	} else {
+		attachment.Content = hex.EncodeToString(content)
+	}
 
 	return 4 + int(len), nil
 }
 
 func (attachment *Message) ToBytes(version uint8) ([]byte, error) {
-	bs, err := restruct.Pack(binary.LittleEndian, attachment)
-	if err != nil {
+	buf := bytes.NewBuffer(nil)
+
+	if err := binary.Write(buf, binary.LittleEndian, attachment.IsTextAndLen); err != nil {
 		return nil, err
 	}
 
-	if version > 0 {
-		return append([]byte{version}, bs...), nil
+	if *attachment.IsText {
+		if err := binary.Write(buf, binary.LittleEndian, []byte(attachment.Content)); err != nil {
+			return nil, err
+		}
+	} else {
+		content, err := hex.DecodeString(attachment.Content)
+		if err != nil {
+			return nil, err
+		}
+
+		if err = binary.Write(buf, binary.LittleEndian, content); err != nil {
+			return nil, err
+		}
 	}
 
-	return bs, nil
+	if version > 0 {
+		return append([]byte{version}, buf.Bytes()...), nil
+	}
+
+	return buf.Bytes(), nil
 }
