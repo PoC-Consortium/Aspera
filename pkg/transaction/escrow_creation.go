2ae588b88633dee4582f6f6472aa02533a6aeb8b
bold <bold@cryptoguru.com>
2018-10-30 08:40:45 +0800

    fix escrow creation

--- pkg/transaction/escrow_creation.go
+++ pkg/transaction/escrow_creation.go
@@ -19,13 +19,14 @@ func (tx *EscrowCreation) WriteAttachmentBytes(e encoding.Encoder) {
 	e.WriteUint32(tx.Attachment.Deadline)
 	e.WriteUint8(uint8(tx.Attachment.DeadlineAction))
 	e.WriteUint8(uint8(tx.Attachment.RequiredSigners))
+	e.WriteUint8(uint8(len(tx.Attachment.Signers)))
 	for _, signer := range tx.Attachment.Signers {
 		e.WriteUint64(signer)
 	}
 }
 
 func (tx *EscrowCreation) AttachmentSizeInBytes() int {
-	return 8 + 4 + 1 + 1 + len(tx.Attachment.Signers)*8
+	return 8 + 4 + 1 + 1 + 1 + len(tx.Attachment.Signers)*8
 }
 
 func (tx *EscrowCreation) GetType() uint16 {
