f02e8a3ad0c146efb8114f07989ae1a160996467
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2018-11-15 21:33:02 -0800

    style(login): style login

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,6 +7,11 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { LoginComponent } from './login.component';
 import { SetupModule } from '../dashboard/setup/setup.module';
+import { MatIconModule, MatInputModule } from '@angular/material';
+import { NotifierModule } from 'angular-notifier';
+import { FormsModule } from '@angular/forms';
+import { I18nModule } from '../../lib/i18n/i18n.module';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
     imports: [
@@ -15,6 +20,12 @@ import { SetupModule } from '../dashboard/setup/setup.module';
         TranslateModule,
         SetupModule,
         routing,
+        MatIconModule,
+        NotifierModule,
+        FormsModule,
+        I18nModule,
+        MatInputModule,
+        NgxMaskModule.forRoot()
     ],
     declarations: [
         LoginComponent
