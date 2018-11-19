6ae131094556e59e12a4c403aa47624cff6d1467
bold <bold@cryptoguru.com>
2018-11-20 06:23:07 +0800

    more digital goods store fixes

--- pkg/transaction/digital_goods_delivery.go
+++ pkg/transaction/digital_goods_delivery.go
@@ -1,6 +1,9 @@
 package transaction
 
 import (
+	"encoding/hex"
+	"math"
+
 	pb "github.com/ac0v/aspera/pkg/api/p2p"
 	"github.com/ac0v/aspera/pkg/encoding"
 )
@@ -16,14 +19,24 @@ type DigitalGoodsDelivery struct {
 
 func (tx *DigitalGoodsDelivery) WriteAttachmentBytes(e encoding.Encoder) {
 	e.WriteUint64(tx.Attachment.Purchase)
-	e.WriteBytesWithInt32Len(tx.Attachment.IsText, tx.Attachment.Data)
-	e.WriteBytes(tx.Attachment.Data)
+	// java wallet <3
+	l := len(tx.Attachment.Data) / 2
+	if tx.Attachment.IsText {
+		e.WriteInt32(int32(l) | math.MinInt32)
+	} else {
+		e.WriteInt32(int32(l))
+	}
+	data := make([]byte, l)
+	if _, err := hex.Decode(data, tx.Attachment.Data); err != nil {
+		return
+	}
+	e.WriteBytes(data)
 	e.WriteBytes(tx.Attachment.Nonce)
 	e.WriteUint64(tx.Attachment.Discount)
 }
 
 func (tx *DigitalGoodsDelivery) AttachmentSizeInBytes() int {
-	return 8 + 4 + len(tx.Attachment.Data) + len(tx.Attachment.Nonce) + 8
+	return 8 + 4 + len(tx.Attachment.Data)/2 + len(tx.Attachment.Nonce) + 8
 }
 
 func (tx *DigitalGoodsDelivery) GetType() uint16 {
