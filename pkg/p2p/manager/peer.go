c6e46cda10ffc6ab5c6e96864c207962a199d063
bold <bold@cryptoguru.com>
2018-11-20 10:32:22 +0800

    set 10 second timeout

--- pkg/p2p/manager/peer.go
+++ pkg/p2p/manager/peer.go
@@ -14,7 +14,7 @@ import (
 
 func init() {
 	// TODO: use dynamic timeout
-	resty.SetTimeout(5 * time.Second)
+	resty.SetTimeout(10 * time.Second)
 }
 
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
